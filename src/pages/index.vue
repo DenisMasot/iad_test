98fc41162ecf408fed98e3b6d3d8d747daa93d98
Denis Masot <denis.masot@hetic.net>
2023-12-07 06:23:18 +0100

    add responsive cols in restaurant list

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -22,12 +22,31 @@ const { data: restaurants, isError } = useFetchRestaurants();
       </a>
     </VAlert>
     <LoadingError v-if="isError" />
-    <div v-else-if="restaurants">
+    <div v-else-if="restaurants"
+      class="card_container"
+    >
       <RestaurantCard
         v-for="restaurant of restaurants"
         :key="restaurant.id"
         :restaurant="restaurant"
+        class="card"
       />
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.card_container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  
+  @media screen and (min-width: 601px) {
+    flex-direction: row;
+    flex-wrap: wrap;
+    .card {
+      flex: 1 1 32%;
+    }
+  }
+}
+</style>
